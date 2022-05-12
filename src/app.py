cc6de57f2782bf5bd400bd9ab73dff103d7f798d
Aimer Neige <aimer.neige.soft@gmail.com>
2022-05-12 21:01:27 +0800

    Removed history, due to sensitive data

---
+++ src/app.py
@@ -0,0 +1,64 @@
+
+'''文本转手写pdf Flask'''
+import os
+import time
+
+from flask_apscheduler import APScheduler
+from flask import Flask, render_template, request, send_file, session, after_this_request
+
+from src.config import Config
+from src.tranisition import txt_to_pdf
+
+app = Flask(__name__)
+'''
+文本转手写pdf api
+method: post
+params: text
+return: pdf
+'''
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    text = request.form['text']
+    if len(text) > 2000:
+        return '文本过长'
+    path = f'{time.time()}+{request.remote_addr}'
+
+    return send_file(txt_to_pdf(text, path))
+
+
+'''定时删除pdf文件'''
+
+
+class ASPConfig(object):
+    JOBS = [
+        {
+            'id': 'job1',  # 一个标识
+            'func': 'app:apscheduler_remove_file',  # 指定运行的函数
+            'args': ["../pdf"],  # 传入函数的参数
+            'trigger': 'interval',  # 指定 定时任务的类型
+            'seconds': 3600  # 运行的间隔时间
+        }
+    ]
+
+    SCHEDULER_API_ENABLED = True
+
+
+def apscheduler_remove_file(path):
+
+    for file in os.listdir(path):
+        if file.endswith('.pdf'):
+            try:
+                os.remove(f'{path}/' + file)
+                print('删除文件'+file)
+            except PermissionError as e:
+                pass
+
+
+if __name__ == '__main__':
+    app.config.from_object(ASPConfig())
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+    scheduler.start()
+    app.run(debug=True)
---
+++ src/tranisition/__init__.py
@@ -0,0 +1,63 @@
+
+'''保存文本为pdf文件'''
+import os
+import threading
+from typing import Iterable, Tuple, Any
+
+import img2pdf
+from PIL import Image
+from handright import handwrite
+
+'''转换pdf文件并返回文件'''
+
+
+def save(pdf_path, images):
+    images[0].save(pdf_path, 'PDF', resolution=100.0, save_all=True, append_images=images[1:])
+    return pdf_path
+
+
+def iterable_to_pdf(iterables: Tuple[Iterable[Image.Image]], pdf_path) -> str:
+    images = []
+    for i in iterables:
+        images.extend([image for image in i])
+    return save(pdf_path, images)
+
+
+from src.config import Config
+
+
+def async_txt(txt):
+    return handwrite(txt, Config.template)
+
+
+class thread_transition(threading.Thread):
+
+    def __init__(self, func, args=()):
+        super(thread_transition, self).__init__()
+        self.result = None
+        self.func = func
+        self.args = args
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        threading.Thread.join(self)
+        try:
+            return self.result
+        except Exception as e:
+            return None
+
+
+def txt_to_pdf(text: str, file_name: str, out_dir: str = Config.out_dir) -> str:
+    cut_count = Config.cut_count
+    if len(text) > cut_count:
+        threads = []
+        for i in range(0, len(text), cut_count):
+            thread = thread_transition(async_txt, (text[i:i + cut_count],))
+            threads.append(thread)
+            thread.start()
+        images = (thread.get_result() for thread in threads)
+    else:
+        images = (handwrite(text, Config.template),)
+    return iterable_to_pdf(images, os.path.join(out_dir, f'{file_name}.pdf'))
